a2882c2c18de5bf83328bf204cbe7dd5f26e636a
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-04 19:38:05 -0300

    Update peliculas.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/Components/peliculas/peliculas.js
+++ src/Components/peliculas/peliculas.js
@@ -3,24 +3,9 @@ import React from 'react'
 // data = Obtener datos de la API
 
 function peliculas() {
-  return (
-    <div className="tabla">
-      <table>
-        <tr>
-          <th>Nombre</th>
-          <th>Genero</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.nombre}</td>
-              <td>{val.genero}</td>
-            </tr>
-          )
-        })}
-      </table>
-    </div>
-  );
+  return(
+    <div>Menu Peliculas</div>
+  )
 }
 
 export default peliculas
