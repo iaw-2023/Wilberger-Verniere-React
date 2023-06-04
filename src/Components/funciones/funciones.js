7866dac750a0c9e34fecfeacafb4e310c72b1647
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-04 19:37:55 -0300

    Update funciones.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/Components/funciones/funciones.js
+++ src/Components/funciones/funciones.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function funciones() {
+function Funciones() {
   return (
-    <div>
-      
-    </div>
+    <div>Menu Funciones</div>
   )
 }
 
-export default funciones
+export default Funciones
