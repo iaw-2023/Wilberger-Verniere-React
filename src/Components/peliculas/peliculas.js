12f91770e11f06a93983f8222a0e90d801622cee
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-04 17:38:43 -0300

    pelicula.js muestra tabla de nombres y generos de peliculas
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/Components/peliculas/peliculas.js
+++ src/Components/peliculas/peliculas.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+// data = Obtener datos de la API
+
 function peliculas() {
   return (
-    <div>
+    <div className="tabla">
+      <table>
+        <tr>
+          <th>Nombre</th>
+          <th>Genero</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.nombre}</td>
+              <td>{val.genero}</td>
+            </tr>
+          )
+        })}
+      </table>
     </div>
-  )
+  );
 }
 
 export default peliculas
