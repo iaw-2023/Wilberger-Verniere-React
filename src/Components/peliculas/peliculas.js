a8debe41f0355105ef382785c04f694c45c1d5a5
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-15 16:26:16 -0300

    Update peliculas.js

--- src/Components/peliculas/peliculas.js
+++ src/Components/peliculas/peliculas.js
@@ -27,7 +27,7 @@ function peliculas() {
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="tabla dark:text-gray-400">
                 <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
-                    <tr className="tablaRow">
+                    <tr>
                         <th scope="col" className="tablaH">Nombre: </th>
                         <th scope="col" className="tablaH">Genero: </th>
                     </tr>
