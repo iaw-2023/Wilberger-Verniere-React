b4ac87737e12b694d00da28ac09913067f03084e
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-15 15:11:10 -0300

    Update generos.js

--- src/Components/generos/generos.js
+++ src/Components/generos/generos.js
@@ -24,31 +24,24 @@ function Generos() {
 
   return (
     <div>
-      <div>Menu Generos</div>
-        <div>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="tabla dark:text-gray-400">
-                    <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
-                        <tr className='tablaRow'>
-                            <th scope="col" className="tablaH">
-                                Nombre:
-                            </th>
-                            <th scope="col" className="tablaH">
-                                Peliculas:
-                            </th>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="tabla dark:text-gray-400">
+                <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
+                    <tr className='tablaRow'>
+                        <th scope="col" className="tablaH">Nombre: </th>
+                        <th scope="col" className="tablaH">Peliculas: </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { genero && genero.length>0 && genero.map((generoObj,index) => (
+                        <tr className="tablaRow">
+                            <th className="tablaH"> {generoObj.Nombre}            </th>
+                            <th className="tablaH"> {generoObj.Peliculas}         </th>
                         </tr>
-                    </thead>
-                    <tbody>
-                        { genero && genero.length>0 && genero.map((generoObj,index) => (
-                            <tr className="tablaRow">
-                                <th className="tablaH"> {generoObj.Nombre}            </th>
-                                <th className="tablaH"> {generoObj.Peliculas}         </th>
-                            </tr>
-                        ))
-                        }
-                    </tbody>
-                </table>
-            </div>
+                    ))
+                    }
+                </tbody>
+            </table>
         </div>
     </div>
   )
