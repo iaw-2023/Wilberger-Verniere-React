34ea270870ff17a29ee0411ed70efc570b627510
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-16 11:04:37 -0300

    Se modifico lo relacionado al carrito
    De acuerdo a los cambios brindados por dataProvider

--- src/Components/carrito/carrito.js
+++ src/Components/carrito/carrito.js
@@ -1,14 +1,86 @@
-import React from 'react'
+import React from 'react';
+import {Button, ButtonGroup} from 'react-bootstrap';
+import "./carrito.css";
+import { dataContext } from '../context/dataContext';
+import { useContext } from 'react';
+    
+export function useComprar(compra, tickets) {
+  const { carrito, setCarrito } = useContext(dataContext);
 
+  setCarrito( () => {
+    compra.NroTickets = tickets;
+    let found = false;
+    let index = 0;
+    console.log("Buscando elemento en carrito");
+    while (!found && index < carrito.length){
+      let aux = carrito.at(index);
+      if (aux.Pelicula == compra.Pelicula && aux.Fecha == compra.Fecha && aux.NroSala == compra.NroSala && aux.Hora == compra.Hora){
+        aux.NroTickets += compra.NroTickets;
+        found = true;
+        console.log("Se encontro en carrito y se sumaron tickets");
+      }
+      index++;
+    }
+    if (!found){
+      console.log("No se encontro, se agrego al carrito");
+      carrito.push(compra);
+    }   
+    console.log(carrito);
+  });
+}
+
+function cancelarOrden(index) { 
+  const { carrito } = useContext(dataContext);
+  carrito.splice(index,1); 
+  console.log(carrito); 
+}
+
+function confirmarCompra(){ console.log("COMPRA CONFIRMADA"); }
+
+function limpiarCompra(){ 
+  const { carrito, setCarrito } = useContext(dataContext);
+  setCarrito( () => {
+    carrito.splice(0,carrito.length); 
+    console.log(carrito);
+  });
+}
 
 function ordenes() {
+  const { carrito } = useContext(dataContext);
   return (
     <div>
-      <div>Aca se muestra la lista de compras a confirmar</div>
-      <form action="https://vercel-deploy-test-7ix687nun-wilbergermatias.vercel.app/rest/compras/crear" method='PUT' enctype="multipart/form-data">
-        <div className='tabla-ordenes'></div>
-        <button type='submit'>Confirmar</button>
-      </form>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="tabla dark:text-gray-400">
+          <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="tablaH">Pelicula:</th>
+              <th scope="col" className="tablaH">Fecha:</th>
+              <th scope="col" className="tablaH">Hora:</th>
+              <th scope="col" className="tablaH">Sala numero:</th>
+              <th scope="col" className="tablaH">Tickets Comprados:</th>
+              <th scope="col" className="tablaH">Accion:</th>
+            </tr>
+          </thead>
+          <tbody>
+            { carrito && carrito.length>0 && carrito.map((compra,index) => (
+              <tr className="tablaRow" key={index}>
+                <th className="tablaH"> {compra.Pelicula}      </th>
+                <th className="tablaH"> {compra.Fecha}         </th>
+                <th className="tablaH"> {compra.Hora}          </th>
+                <th className="tablaH"> {compra.NroSala}       </th>
+                <th className="tablaH"> {compra.NroTickets}    </th>
+                <th className="tablaH"> 
+                    <Button className="quitar-orden" onClick={ ()=>cancelarOrden(index) }>Quitar</Button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <ButtonGroup className='boton-group'>
+        <Button className="boton-cancelar" onClick={ ()=>limpiarCompra() }>Eliminar Compra</Button>
+        <Button className="boton-enviar" onClick={ ()=>confirmarCompra() }>Confirmar Compra</Button>
+      </ButtonGroup>
     </div>
   )
 }
--- src/Components/funciones/funciones.js
+++ src/Components/funciones/funciones.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import './funciones.css';
+import {Button} from 'react-bootstrap';
 import axios from 'axios';
+import {useComprar} from '../carrito/carrito';
  
 
-function Funciones() {
+function handleComprar(props){
+    var p = prompt("Ingrese la cantidad de tickets que desea comprar", "0");
+    var cantTickets = parseInt(p);
+    if (!cantTickets<1){
+        console.log("se va a llamar a comprar");
+        useComprar(props, cantTickets);
+        console.log("se completo comprar");
+    }
+}
 
+function Funciones() {
     const [funcion, setFuncion] = useState([])
     const [error, setError] = useState(null);
 
@@ -21,43 +32,36 @@ function Funciones() {
         fetchFuncion();
     },[]);
 
-    if (error) return<p> OCURRIO UN ERROR AL PEDIR LAS FUNCIONES</p>
+    if (error) return<p>OCURRIO UN ERROR AL PEDIR LAS FUNCIONES</p>
     
     return (
         <div>
-            <div>Menu Funciones</div>
-            <div>
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="tabla dark:text-gray-400">
-                        <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="tablaH">
-                                    Pelicula:
-                                </th>
-                                <th scope="col" className="tablaH">
-                                    Fecha:
-                                </th>
-                                <th scope="col" className="tablaH">
-                                    Hora:
-                                </th>
-                                <th scope="col" className="tablaH">
-                                    Sala numero:
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="tabla dark:text-gray-400">
+                    <thead className="tablaHead dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="tablaH">Pelicula:</th>
+                            <th scope="col" className="tablaH">Fecha:</th>
+                            <th scope="col" className="tablaH">Hora:</th>
+                            <th scope="col" className="tablaH">Sala numero:</th>
+                            <th scope="col" className="tablaH">Accion:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { funcion && funcion.length>0 && funcion.map((funcionObj,index) => (
+                            <tr className="tablaRow" key={index}>
+                                <th className="tablaH"> {funcionObj.Pelicula}      </th>
+                                <th className="tablaH"> {funcionObj.Fecha}         </th>
+                                <th className="tablaH"> {funcionObj.Hora}          </th>
+                                <th className="tablaH"> {funcionObj.NroSala}       </th>
+                                <th className="tablaH"> 
+                                    <Button className="añadir-ticket" onClick={ ()=>handleComprar(funcionObj) }>Comprar</Button>
                                 </th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            { funcion && funcion.length>0 && funcion.map((funcionObj,index) => (
-                                <tr className="tablaRow">
-                                    <th className="tablaH"> {funcionObj.Pelicula}      </th>
-                                    <th className="tablaH"> {funcionObj.Fecha}         </th>
-                                    <th className="tablaH"> {funcionObj.Hora}          </th>
-                                    <th className="tablaH"> {funcionObj.NroSala}       </th>
-                                </tr>
-                            ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                        ))
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
