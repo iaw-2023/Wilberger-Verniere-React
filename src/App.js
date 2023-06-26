e16f9cf5d136322fd87ddcb364e43757a214d45b
WilbergerMatias <matias180399@gmail.com>
2023-06-26 13:06:30 -0300

    correccion de error en App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Welcome from './Components/welcome/welcome'
 import Funciones from './Components/funciones/funciones'
@@ -8,30 +8,32 @@ import Carrito from './Components/carrito/carrito'
 import NavBar from './Components/navbar/navbar'
 import Footer from './Components/footer/footer'
 import DataProvider from './Components/context/dataContext'
-import Compras from './Components/compras/compras'
+import Compras from './Components/compras/compras.js'
 import FuncionesAsociadas from './Components/funciones/funciones-asociadas'
 import ComprasAsociadas from './Components/compras/comprasAsociadas'
 
 function App() {
   return (
-    <DataProvider>
-      <div className="navigation-menu">
-        <NavBar/>
-      </div>
-      <Routes>
-        <Route path="/" element={ <Welcome/>} />
-        <Route path="/funciones" element={ <Funciones/> } />
-        <Route path="/funcionesAsociadas" element={ <FuncionesAsociadas/> } />
-        <Route path="/peliculas" element={<Peliculas/>} />
-        <Route path="/generos" element={<Generos/>} />
-        <Route path="/compras" element={<Compras/>} />
-        <Route path="/comprasAsociadas" element={ <ComprasAsociadas/> } />
-        <Route path="/carrito" element={<Carrito/>} />
-      </Routes>
-      {/* <div className= "page-footer">
-        <Footer/>
-      </div> */}
-    </DataProvider>
+    <div>
+      <DataProvider>
+        <div className="navigation-menu">
+          <NavBar/>
+        </div>
+        <Routes>
+          <Route path="/" element={ <Welcome/>} />
+          <Route path="/funciones" element={ <Funciones/> } />
+          <Route path="/funcionesAsociadas" element={ <FuncionesAsociadas/> } />
+          <Route path="/peliculas" element={<Peliculas/>} />
+          <Route path="/generos" element={<Generos/>} />
+          <Route path="/compras" element={<Compras/>} />
+          <Route path="/comprasAsociadas" element={ <ComprasAsociadas/> } />
+          <Route path="/carrito" element={<Carrito/>} />
+        </Routes>
+        {/* <div className= "page-footer">
+          <Footer/>
+        </div> */}
+      </DataProvider>
+    </div>
   )
 }
 
