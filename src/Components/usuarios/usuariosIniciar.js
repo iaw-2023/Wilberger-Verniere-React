9f546f2c5529b44429427134007c9221bfc75060
MartinVerniere <martincho7verniere@yahoo.com>
2023-08-26 18:48:07 -0300

    Se agregaron paginas para hacer Login o Registrarse
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

---
+++ src/Components/usuarios/usuariosIniciar.js
@@ -0,0 +1,57 @@
+import "./usuarios.css";
+import "../../master.css";
+import { useContext, useState } from "react";
+import { dataContext } from "../context/dataContext";
+
+function usuarios() {
+
+    const [contraseña, setContraseña] = useState("");
+    const [email, setEmail] = useState("");
+    const { setUsuarioActivo, setEmailActivo } = useContext(dataContext);
+
+    const fetchUsuario = () =>
+    {
+        console.log("Busco usuario:",contraseña, email);
+        /* return axios.get('https://wilberger-verniere-laravel-zxwy.vercel.app/rest/usuarios/iniciar', {
+        params: {
+            'Email': email,
+            'Contraseña': contraseña,
+        }
+        })
+        .then((response) => {
+            setUsuarioActivo(response.data.data);
+            setEmailActivo(response.data.data);
+            setError(null);
+        }).catch(setError); */
+    }
+
+    const handleTextEmail = (event) => 
+    {
+        event.preventDefault();
+        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    const handleTextContraseña = (event) => 
+    {
+        event.preventDefault();
+        console.log(event.target.value);
+        setContraseña(event.target.value);
+    }
+
+    return (
+        <div>
+            <div className="email">
+                Email: 
+                <input type="text" className="input-user-emailUser" value={email} onChange={handleTextEmail}/>
+            </div> 
+            <div className="contraseña">
+                Contraseña: 
+                <input type="text" className="input-user-contraseñaUser" value={contraseña} onChange={handleTextContraseña}/>
+            </div>
+            { email && contraseña && <button className="boton-enviar" onClick={ fetchUsuario() }>Confirmar</button>}
+        </div>
+    )
+}
+
+export default usuarios
---
+++ src/Components/usuarios/usuariosRegistrar.js
@@ -0,0 +1,79 @@
+import "./usuarios.css";
+import "../../master.css";
+import { useState } from "react";
+
+function usuarios() {
+
+    const [nombreUser, setNombreUser] = useState("");
+    const [contraseña, setContraseña] = useState("");
+    const [email, setEmail] = useState("");
+    const [emailValido, SetEmailValido] = useState(false);
+
+    const guardarUsuario = () =>
+    {
+        console.log("Creo usuario:", nombreUser, contraseña, email);
+        /* return axios.get('https://wilberger-verniere-laravel-zxwy.vercel.app/rest/usuarios/crear', {
+        params: {
+            'Email': email,
+            'Contraseña': contraseña,
+            'Usuario': nombreUser
+        }
+        })
+        .then((response) => {
+            // SI ES EXITOSO
+                setEmailActivo(response.data.data);
+                setUsuarioAcitvo(response.data.data);
+                setError(null);
+        }).catch(setError); */
+    }
+
+    const handleTextNombreUser = (event) => 
+    {
+        event.preventDefault();
+        console.log(event.target.value);
+        setNombreUser(event.target.value);
+    }
+
+    const handleTextEmail = (event) => 
+    {
+        event.preventDefault();
+        console.log(event.target.value);
+        setEmail(event.target.value);
+        if (esValidoEmail(event.target.value)){
+            SetEmailValido(true);
+          }
+        else { SetEmailValido(false); }
+    }
+
+    const esValidoEmail = (email) => {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    const handleTextContraseña = (event) => 
+    {
+        event.preventDefault();
+        console.log(event.target.value);
+        setContraseña(event.target.value);
+    }
+
+    return (
+        <div>
+            <div className="usuario">
+                Nombre de Usuario: 
+                <input type="text" className="input-user-nombreUser" value={nombreUser} onChange={handleTextNombreUser}/>
+            </div>
+            <div className="email">
+                Email: 
+                <input type="text" className="input-user-emailUser" value={email} onChange={handleTextEmail}/>
+                { email && (!emailValido) && <div className='email-valido-text'>Este email no es valido!</div>}
+            </div> 
+            <div className="contraseña">
+                Contraseña:
+                <input type="text" className="input-user-contraseñaUser" value={contraseña} onChange={handleTextContraseña}/>
+            </div>
+            { emailValido && nombreUser && contraseña && <button className="boton-enviar" onClick={ guardarUsuario() }>Confirmar</button>}
+        </div>
+    )
+}
+
+export default usuarios
