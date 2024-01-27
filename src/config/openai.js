86da09e1794823652ce9769f736de25fbf25b1ce
MartinVerniere <martincho7verniere@yahoo.com>
2024-01-27 18:44:44 -0300

    Update openai.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/config/openai.js
+++ src/config/openai.js
@@ -1,3 +1,3 @@
-const OPENAI_API_KEY = 'sk-BzJcdZNMPOd1CoUtZzx2T3BlbkFJygSmfaduUBN7glBFaXoGZ';
+const OPENAI_API_KEY = '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';
 
 export default OPENAI_API_KEY;
