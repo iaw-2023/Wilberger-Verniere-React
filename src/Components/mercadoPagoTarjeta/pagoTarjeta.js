2a546aef32554c460d32bafdeeeeddc900b0fed2
MartinVerniere <martincho7verniere@yahoo.com>
2024-07-06 15:34:52 -0300

    Modificada forma de llamar al script de mercado pago
    Y de como mostra la pagina de pagoTarjeta

--- src/Components/mercadoPagoTarjeta/pagoTarjeta.js
+++ src/Components/mercadoPagoTarjeta/pagoTarjeta.js
@@ -3,64 +3,76 @@ import MERCADOPAGO_API_KEY from "../../config/mercadopago";
 //https://github.com/mercadopago/sdk-js/blob/main/API/bricks/card-payment.md VER PAGINA EJEMPLO
 function pagoTarjeta() {
 
-    <script src="https://sdk.mercadopago.com/js/v2"></script>
+    useEffect(() => {
+      const scriptMercadoPago = document.createElement("script");
+      scriptMercadoPago.src = "https://sdk.mercadopago.com/js/v2";
+      scriptMercadoPago.async = true;
+      scriptMercadoPago.onload = () => initializeBrick();
+      document.body.appendChild(scriptMercadoPago);
+  
+      return () => {
+        document.body.removeChild(scriptMercadoPago);
+      };
+    }, []);
 
-    const mp = new MercadoPago(MERCADOPAGO_API_KEY, { locale: 'es-AR' });
-    const brickCardPayment = mp.bricks().create("cardPayment", "cardPaymentBrick_container", {
-        initialization: {
-          amount: 100,
-          payer: {
-            email: "",
-          },
-        },
-        callbacks: {
-          onReady: () => {
-            // handle form ready
-          },
-          onSubmit: (cardData) => {
-            return new Promise((resolve, reject) => {
-              fetch("/process_payment", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(cardData),
-              })
-                .then((response) => response.json())
-                .then((response) => {
-                  // get payment result
-                  console.log("Resolve: ",response)
-                  resolve();
-                })
-                .catch((error) => {
-                  // get payment result error
-                  console.log("Error pago: ",error);
-                  reject();
-                });
-            });
-          },
-          onError: (error) => {
-            // handle error
-            console.log("Error :",error);
-          },
-        },
-        customization: {
-            visual: {
-              style: {
-                customVariables: {
-                  theme: 'default',
-                }
-              }
+    const initializeBrick = () => {
+      const mp = new MercadoPago(MERCADOPAGO_API_KEY, { locale: 'es-AR' });
+      mp.bricks().create("cardPayment", "cardPaymentBrick_container", {
+          initialization: {
+            amount: 100,
+            payer: {
+              email: "",
             },
-            paymentMethods: {
-                maxInstallments: 1,
-            }
-        },
+          },
+          callbacks: {
+            onReady: () => {
+              // handle form ready
+            },
+            onSubmit: (cardData) => {
+              return new Promise((resolve, reject) => {
+                fetch("/process_payment", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(cardData),
+                })
+                  .then((response) => response.json())
+                  .then((response) => {
+                    // get payment result
+                    console.log("Resolve: ",response)
+                    resolve();
+                  })
+                  .catch((error) => {
+                    // get payment result error
+                    console.log("Error pago: ",error);
+                    reject();
+                  });
+              });
+            },
+            onError: (error) => {
+              // handle error
+              console.log("Error :",error);
+            },
+          },
+          customization: {
+              visual: {
+                style: {
+                  customVariables: {
+                    theme: 'default',
+                  }
+                }
+              },
+              paymentMethods: {
+                  maxInstallments: 1,
+              }
+          },
       });
+    } 
 
     return(
         <div>
-            { brickCardPayment }
+             <div id="cardPaymentBrick_container"></div>
         </div>
     )
 }
