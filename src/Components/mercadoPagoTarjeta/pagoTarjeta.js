1ec631135f6aedcafa3ccd20de7f01ac57a4fefd
MartinVerniere <martincho7verniere@yahoo.com>
2024-07-20 18:00:19 -0300

    Update pagoTarjeta.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/Components/mercadoPagoTarjeta/pagoTarjeta.js
+++ src/Components/mercadoPagoTarjeta/pagoTarjeta.js
@@ -1,100 +1,100 @@
 import MERCADOPAGO_API_KEY from "../../config/mercadopago";
 import React, { useContext, useEffect } from 'react';
 import { dataContext } from '../context/dataContext';
+import apiClient from "../../Services/api";
 
 function PagoTarjeta() {
 
-    const { confirmarCompra, observacionesCompra } = useContext(dataContext);
-    const inicializacionMercadoPago = () =>
-    {
-      const scriptMercadoPago = document.createElement("script");
-      scriptMercadoPago.src = "https://sdk.mercadopago.com/js/v2";
-      scriptMercadoPago.async = true;
-      scriptMercadoPago.onload = () => initializeBrick();
-      document.body.appendChild(scriptMercadoPago);
+  const { confirmarCompra, observacionesCompra } = useContext(dataContext);
+  const inicializacionMercadoPago = () =>
+  {
+    const scriptMercadoPago = document.createElement("script");
+    scriptMercadoPago.src = "https://sdk.mercadopago.com/js/v2";
+    scriptMercadoPago.async = true;
+    scriptMercadoPago.onload = () => initializeBrick();
+    document.body.appendChild(scriptMercadoPago);
+
+    return () => {
+      document.body.removeChild(scriptMercadoPago);
+    };
+  }
+  const getCurrentDate = (separator='-') => 
+  {
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+      
+    return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`;
+  }
   
-      return () => {
-        document.body.removeChild(scriptMercadoPago);
-      };
-    }
-    const getCurrentDate = (separator='-') => 
-    {
-      let newDate = new Date()
-      let date = newDate.getDate();
-      let month = newDate.getMonth() + 1;
-      let year = newDate.getFullYear();
-        
-      return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`;
-    }
-    
 
-    useEffect(() => {
-      inicializacionMercadoPago();
-    }, []);
+  useEffect(() => {
+    inicializacionMercadoPago();
+  }, []);
 
-    const initializeBrick = () => {
-      const mp = new MercadoPago(MERCADOPAGO_API_KEY, { locale: 'es-AR' });
-      mp.bricks().create("cardPayment", "cardPaymentBrick_container", {
-          initialization: {
-            amount: 100,
-            payer: {
-              email: sessionStorage.getItem('userEmail'),
-            },
+  const initializeBrick = () => {
+    const mp = new MercadoPago(MERCADOPAGO_API_KEY, { locale: 'es-AR' });
+    mp.bricks().create("cardPayment", "cardPaymentBrick_container", {
+        initialization: {
+          amount: 100,
+          payer: {
+            email: sessionStorage.getItem('userEmail'),
           },
-          callbacks: {
-            onReady: () => {
-              // handle form ready
-            },
-            onSubmit: (cardData) => {
-              return new Promise((resolve, reject) => {
-                fetch("/process_payment", {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify(cardData),
+        },
+        callbacks: {
+          onReady: () => {
+            // handle form ready
+          },
+          onSubmit: (cardData) => {
+            return new Promise((resolve, reject) => {
+              fetch("http://localhost:8000/process_payment", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(cardData),
+              })
+                .then((response) => response.json())
+                .then((response) => {
+                  // get payment result
+                  console.log("Resolve: ",response)
+                  resolve();
+                  console.log("Se confirma la compra con tarjeta");
+                  confirmarCompra(observacionesCompra + "- PAGO TARJETA MP", sessionStorage.getItem('userEmail'), getCurrentDate());
                 })
-                  .then((response) => console.log(response.text))
-                  .then((response) => response.json())
-                  .then((response) => {
-                    // get payment result
-                    console.log("Resolve: ",response)
-                    resolve();
-                    console.log("Se confirma la compra con tarjeta");
-                    confirmarCompra(observacionesCompra + "- PAGO TARJETA MP", sessionStorage.getItem('userEmail'), getCurrentDate());
-                  })
-                  .catch((error) => {
-                    // get payment result error
-                    console.log("Error pago: ",error);
-                    reject();
-                  });
-              });
-            },
-            onError: (error) => {
-              // handle error
-              console.log("Error :",error);
-            },
+                .catch((error) => {
+                  // get payment result error
+                  console.log("Error pago: ",error);
+                  reject();
+                });
+            });
           },
-          customization: {
-              visual: {
-                style: {
-                  customVariables: {
-                    theme: 'default',
-                  }
+          onError: (error) => {
+            // handle error
+            console.log("Error :",error);
+          },
+        },
+        customization: {
+            visual: {
+              style: {
+                customVariables: {
+                  theme: 'default',
                 }
-              },
-              paymentMethods: {
-                  maxInstallments: 1,
               }
-          },
-      });
-    }
+            },
+            paymentMethods: {
+                maxInstallments: 1,
+            }
+        },
+    });
+  }
 
-    return(
-        <div>
-             <div id="cardPaymentBrick_container"></div>
-        </div>
-    )
+  return(
+      <div>
+            <div id="cardPaymentBrick_container"></div>
+      </div>
+  )
 }
 
 export default PagoTarjeta
