7d434b85d30bfb70caabca093cd60a665eb67734
MartinVerniere <martincho7verniere@yahoo.com>
2024-08-02 17:46:36 -0300

    Se añadieron try catch al SW
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,39 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('my-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/app/layout.tsx','/app/page.tsx',
-        '/src/Components/carrito/carrito.js',
-        '/src/Components/compras/compras.js', '/src/Components/compras/comprasAsociadas.js',
-        '/src/Components/funciones/funciones.js','/src/Components/funciones/funciones-asociadas.js',
-        '/src/Components/generos/generos.js',
-        '/src/Components/mercadoPagoTarjeta/pagoTarjeta.js',
-        '/src/Components/navbar/navbar.js',
-        '/src/Components/peliculas/peliculas.js','/src/Components/peliculas/peliculas-informacion.js',
-        '/src/Components/slider/slider.js',
-        '/src/Components/usuarios/usuariosIniciar.js', '/src/Components/usuarios/usuariosRegistrar.js',
-        '/src/Components/welcome/welcome.js',
-        '/public/icons/webCinesIcon.png'
-      ]);
-    })
-  );
+  try{
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/app/layout.tsx','/app/page.tsx',
+          '/src/Components/carrito/carrito.js',
+          '/src/Components/compras/compras.js', '/src/Components/compras/comprasAsociadas.js',
+          '/src/Components/funciones/funciones.js','/src/Components/funciones/funciones-asociadas.js',
+          '/src/Components/generos/generos.js',
+          '/src/Components/mercadoPagoTarjeta/pagoTarjeta.js',
+          '/src/Components/navbar/navbar.js',
+          '/src/Components/peliculas/peliculas.js','/src/Components/peliculas/peliculas-informacion.js',
+          '/src/Components/slider/slider.js',
+          '/src/Components/usuarios/usuariosIniciar.js', '/src/Components/usuarios/usuariosRegistrar.js',
+          '/src/Components/welcome/welcome.js',
+          '/public/icons/webCinesIcon.png'
+        ]);
+      })
+    );
+  } catch (error){
+    console.log("Error en addAll: ", error);
+  }
+  
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  try {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+  catch (error) {
+    console.log("Error en fetch: ", error)
+  }
 });
