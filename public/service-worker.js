2e8fc75ec4f46dadaa061ed44847761cfb7c170d
MartinVerniere <martincho7verniere@yahoo.com>
2024-08-02 17:42:42 -0300

    Se elimino index.js innecesario
    y se cambiaron paths en addAll en el SW
    
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,19 +2,19 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-cache').then(cache => {
       return cache.addAll([
-        './',
-        './app/layout.tsx','./app/page.tsx','./app/index.js',
-        './src/Components/carrito/carrito.js',
-        './src/Components/compras/compras.js', './src/Components/compras/comprasAsociadas.js',
-        './src/Components/funciones/funciones.js','./src/Components/funciones/funciones-asociadas.js',
-        './src/Components/generos/generos.js',
-        './src/Components/mercadoPagoTarjeta/pagoTarjeta.js',
-        './src/Components/navbar/navbar.js',
-        './src/Components/peliculas/peliculas.js','./src/Components/peliculas/peliculas-informacion.js',
-        './src/Components/slider/slider.js',
-        './src/Components/usuarios/usuariosIniciar.js', './src/Components/usuarios/usuariosRegistrar.js',
-        './src/Components/welcome/welcome.js',
-        './public/icons/webCinesIcon.png'
+        '/',
+        '/app/layout.tsx','/app/page.tsx',
+        '/src/Components/carrito/carrito.js',
+        '/src/Components/compras/compras.js', '/src/Components/compras/comprasAsociadas.js',
+        '/src/Components/funciones/funciones.js','/src/Components/funciones/funciones-asociadas.js',
+        '/src/Components/generos/generos.js',
+        '/src/Components/mercadoPagoTarjeta/pagoTarjeta.js',
+        '/src/Components/navbar/navbar.js',
+        '/src/Components/peliculas/peliculas.js','/src/Components/peliculas/peliculas-informacion.js',
+        '/src/Components/slider/slider.js',
+        '/src/Components/usuarios/usuariosIniciar.js', '/src/Components/usuarios/usuariosRegistrar.js',
+        '/src/Components/welcome/welcome.js',
+        '/public/icons/webCinesIcon.png'
       ]);
     })
   );
