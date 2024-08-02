95e9c3cdaf9941410a733b76a1ec109e8d71ad29
MartinVerniere <martincho7verniere@yahoo.com>
2024-08-02 18:53:59 -0300

    Update service-worker.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,33 @@
-self.addEventListener('install', event => {
-  try{
-    event.waitUntil(
-      caches.open('my-cache').then(cache => {
+const STATIC_CACHE = 'static-cache-v1';
+const PAGES_CACHE = 'pages-cache-v1';
+const CSS_CACHE = 'css-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.open(STATIC_CACHE).then((cache) => {
         return cache.addAll([
-          '/',
+          '/'
         ]);
+      }),
+      caches.open(PAGES_CACHE).then((cache) => {
+        return cache.addAll([
+          '/app/page.tsx'
+        ])
+        .catch(error => {
+          console.log("Error PAGES_CACHE: ", error)
+        });
+      }),
+      caches.open(CSS_CACHE).then((cache) => {
+        return cache.addAll([
+          '/app/globals.css'
+        ])
+        .catch(error => {
+          console.log("Error CSS_CACHE: ", error)
+        });
       })
-    );
-  } catch (error){
-    console.log("Error en addAll: ", error);
-  }
-  
+    ])
+  );
 });
 
 self.addEventListener('fetch', event => {
