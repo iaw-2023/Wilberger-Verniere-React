86cadf0d9ef5981d8414529404c2a063384eee6f
MartinVerniere <martincho7verniere@yahoo.com>
2024-08-24 16:29:29 -0300

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
  
 const apiClient = axios.create({
     //baseURL: 'https://wilberger-verniere-laravel-zxwy.vercel.app/',
-    baseURL: 'https://wilberger-verniere-laravel-zxwy-qwd3r9b8t-iawv.vercel.app/',
+    baseURL: 'https://wilberger-verniere-laravel-zxwy-q6r2jt027-iawv.vercel.app/',
     withCredentials: true,
 });
  
