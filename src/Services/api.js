eae2f8f227123cb13625c051d921fc8507778db5
MartinVerniere <martincho7verniere@yahoo.com>
2024-08-24 17:15:07 -0300

    Update api.js
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
  
 const apiClient = axios.create({
     //baseURL: 'https://wilberger-verniere-laravel-zxwy.vercel.app/',
-    baseURL: 'https://wilberger-verniere-laravel-zxwy-otomov33w-iawv.vercel.app/',
+    baseURL: 'https://wilberger-verniere-laravel-zxwy-lllu2nx7s-iawv.vercel.app/',
     withCredentials: true,
 });
  
