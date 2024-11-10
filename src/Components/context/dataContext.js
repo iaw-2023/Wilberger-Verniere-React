9cff489361cd6508fd999f415f8a931f0f3055c2
MartinVerniere <martincho7verniere@yahoo.com>
2024-11-10 16:30:05 -0300

    Update dataContext.js

--- src/Components/context/dataContext.js
+++ src/Components/context/dataContext.js
@@ -181,7 +181,7 @@ const DataProvider = ( {children} ) => {
     const obtenerInfoPeliculaGemini = (nombre) => 
     {
         setRespuestaGemini('');
-        const consulta = "¿Can you give me a short plot description of the movie: "+nombre+"?. If not posible respond 'Error al buscar una sinopsis en Gemini'. The response should be formatted like a JSON file where it should have parameters >>> content: <Here goes plot description only>, and only show if there is plot described, else dont add this atribute | error: <Here goes Error al buscar una sinopsis en Gemini> and only show if there is no plot described in answer, else dont add this atribute";
+        const consulta = "¿Can you give me a short plot description of the movie: "+nombre+"?. If not posible respond 'Error al buscar una sinopsis en Gemini'. The response should be formatted like a JSON file where it should have parameters >>> content: <Here goes plot description only>, and only show if there if its plot is described, else dont add this atribute | error: <Here goes Error al buscar una sinopsis en Gemini> and only show if there is no plot described in answer, else dont add this atribute";
         preguntarGeminiAPI(consulta);
     }
     
@@ -195,10 +195,10 @@ const DataProvider = ( {children} ) => {
                 }
             });
             console.log('Respuesta Gemini: ',response);
-            const responseAsJSON = JSON.parse(response.data);
-
-            if (responseAsJSON.status === 200) {
-                const sinopsis = responseAsJSON.data.content || responseAsJSON.data.sinopsis || 'No se encontró sinopsis';
+            
+            if (response.status === 200) {
+                const responseAsJSON = JSON.parse(response.data.content);
+                const sinopsis = responseAsJSON.content;
                 setRespuestaGemini(sinopsis);
                 setErrorRespuestaGemini('');
             } else {
